faca0b42450c03727c13e8d88bd54badbf5bce64
Roderick Monje <rod@foveacentral.com>
2017-10-13 11:51:19 -0400

    spec: removed back-ticks from table name

--- spec/lib/percona_ar/query_builder_spec.rb
+++ spec/lib/percona_ar/query_builder_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe PerconaAr::QueryBuilder do
 
       it "leaves sql unchanged" do
         is_expected.to receive(:new).
-          with("`users`", /DROP INDEX `index_users_on_email`/i, ActiveRecord::Base.connection).
+          with("users", /DROP INDEX `index_users_on_email`/i, ActiveRecord::Base.connection).
           and_call_original
       end
     end
