fc78c849d3926e766d912b979b8d784013a0f238
Roderick Monje <rod@foveacentral.com>
2017-10-13 12:24:26 -0400

    spec: covered CREATE INDEX

--- spec/lib/percona_ar/query_builder_spec.rb
+++ spec/lib/percona_ar/query_builder_spec.rb
@@ -83,5 +83,15 @@ RSpec.describe PerconaAr::QueryBuilder do
           and_call_original
       end
     end
+
+    context "when sql has alter statement with CREATE INDEX" do
+      let(:sql) { "CREATE  INDEX `index_users_on_email`  ON `users` (`email`)" }
+
+      it "converts sql to valid ALTER ADD INDEX statement" do
+        is_expected.to receive(:new).
+          with("users", /ADD INDEX (`email`) index_users_on_email/i, ActiveRecord::Base.connection).
+          and_call_original
+      end
+    end
   end
 end
